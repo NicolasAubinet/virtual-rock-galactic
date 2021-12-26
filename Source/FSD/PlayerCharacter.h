4aaba50ade6689643a7398b743c417ce8003213e
Nicolas Aubinet <n.aubinet@gmail.com>
2021-12-26 21:04:08 +0100

    Call ForceIsPressingMovementInputKey to make the other players see the movement animation (this toggle a server-side flag that dispatches the walking animation)

--- Source/FSD/PlayerCharacter.h
+++ Source/FSD/PlayerCharacter.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -47,4 +45,8 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite) UWidgetInteractionComponent* WidgetInteraction;
 	
-	UFUNCTION(BlueprintCallable) AItem* GetEquippedItem() { return nullptr; };};
+	UFUNCTION(BlueprintCallable) AItem* GetEquippedItem() { return nullptr; }
+
+	UFUNCTION(BlueprintCallable) bool IsMovementInputPressed() { return false; }
+	UFUNCTION(BlueprintCallable) void ForceIsPressingMovementInputKey() {}
+};
